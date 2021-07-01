b813d7020ad7d2887cbcd176a06acbc9195fb739
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-07-01 23:03:40 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,11 @@
+#This is a simple calculator
+a=float(input("Enter first number: "));
+b=float(input("Enter second number: "));
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+print("Sum is ",sum);
+print("Subtraction is ",sub);
+print("Multiplication is ",mul);
+print("Divide is ",div);
